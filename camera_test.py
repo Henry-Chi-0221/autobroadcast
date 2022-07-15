a1c3d6481b6e75c26f8e3272ce5884e329376a2f
Henry Chi <48129098+Henry-Chi-0221@users.noreply.github.com>
2022-07-15 11:21:49 +0800

    Stereo cam recorder added.

--- camera_test.py
+++ camera_test.py
@@ -2,21 +2,40 @@ import cv2
 import numpy as np
 from time import time 
 
-cap_1 = cv2.VideoCapture(1)
+cap_1 = cv2.VideoCapture(0)
 cap_2 = cv2.VideoCapture(2)
+cap_1.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap_1.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap_2.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap_2.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out_1 = cv2.VideoWriter('out_R.mp4', fourcc, 10.0, (1920,  1080))
+out_2 = cv2.VideoWriter('out_L.mp4', fourcc, 10.0, (1920,  1080))
+counter = 0
 while(cap_1.isOpened() and cap_2.isOpened()):
     start = time()
     ret_1 , frame_1 = cap_1.read()
     ret_2 , frame_2 = cap_2.read()
-    print(frame_1.shape , type(frame_1))
-    print(frame_2.shape , type(frame_2))
-    print("\m")
+    #print(frame_1.shape , type(frame_1))
+    #print(frame_2.shape , type(frame_2))
+    #print("\m")
+
+    out_1.write(frame_1)
+    out_2.write(frame_2)
+    counter += 1
+    #print(counter//10)
+    if counter > 10 * 60:
+        break
     cv2.imshow('frame_1',frame_1)
-    cv2.imshow('frame_1',frame_2)
+    cv2.imshow('frame_2',frame_2)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
     fps = round (1 / (time() - start)  ,3) 
     #print(fps)
-    
+cap_1.release()
+cap_2.release()
+out_1.release()
+out_2.release()
+cv2.destroyAllWindows()
 #<class 'numpy.ndarray'> (1080, 3840, 3)
