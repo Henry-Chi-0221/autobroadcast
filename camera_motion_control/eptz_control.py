ePTZ fix : adaptive camera/full size, targeting test/parameter adjustment, moving average design for homography matrix

camera_motion_control/eptz_control.py
@@ -5,21 +5,22 @@ import random
 import numpy as np
 
 class eptz(object):
-    def __init__(self ,size,fullsize):
+    def __init__(self ,size,fullsize ,kp,ki,kd,debug=False):
         self.width , self.height = size
         self.full_width ,self.full_height = fullsize
         #self.width = int(width)
         #self.height = int(height)
 
         # PID parameters
-        self.kp = 0.005
-        self.ki = 0.5
-        self.kd = 0.1
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
 
         self.current_x = self.width//2
         self.current_y = self.height//2
         self.current_zoom = 1
 
+        self.debug = debug
     def pid(self,target,current):
         des_point = target
         pid = PID( 
@@ -33,7 +34,6 @@ class eptz(object):
         return pid_output
             
     def zoom(self,zoom_ratio = 1  , x_offset =0 , y_offset = 0):
-        x_center,y_center = self.width//2 , self.height//2
         width = self.width//zoom_ratio
         height = self.height//zoom_ratio
         x_center = x_offset
@@ -50,30 +50,29 @@ class eptz(object):
             self.current_x += pid_x
         
         pid_y = self.pid(y_pos , self.current_y) # target current
-        if  self.height*0.5*(1/self.current_zoom) < self.current_y + pid_y < self.height*(1-0.5*(1/self.current_zoom)):
+        if  self.height*0.5*(1/self.current_zoom) < self.current_y + pid_y < self.full_height - self.height * (0.5*(1/self.current_zoom)):
             self.current_y += pid_y
-        
+            
         pid_zoom = self.pid(zoom_ratio , self.current_zoom)
         
         #here !!!
         if  (0 < self.current_zoom + pid_zoom < 3) and \
             (self.current_x - self.width*0.5*(1/self.current_zoom) )>= 0  and \
-            (self.current_x + self.width*0.5*(1/self.current_zoom) )<= self.width * 2 and\
+            (self.current_x + self.width*0.5*(1/self.current_zoom) )<= self.full_width and\
             self.current_y - self.height*0.5*(1/self.current_zoom) >= 0  and \
-            self.current_y + self.height*0.5*(1/self.current_zoom) <= self.height:
+            self.current_y + self.height*0.5*(1/self.current_zoom) <= self.full_height :
 
             self.current_zoom += pid_zoom
         
         x1,y1,x2,y2 = self.zoom(zoom_ratio=self.current_zoom,x_offset=self.current_x,y_offset=self.current_y) #x
         res_x = np.clip(np.array([x1,x2]) , 0 , self.full_width)
-        res_y = np.clip(np.array([y1,y2]) , 0 , self.height)
+        res_y = np.clip(np.array([y1,y2]) , 0 , self.full_height)
         (x1,x2), (y1,y2) = res_x[:] , res_y[:]
+        if self.debug:
+            cv2.rectangle(img , (x1,y1) , (x2,y2) , (0,255,255) , 2)
+            cv2.circle(img , (int(x_pos) , int(y_pos)) , 15 ,(0,255,0) , -1)
+            cv2.circle(img , ((x1+x2)//2 , (y1+y2)//2) , 15 ,(255,255,255) , 2)
         
-        cv2.rectangle(img , (x1,y1) , (x2,y2) , (0,255,255) , 2)
-        cv2.circle(img , (int(x_pos) , int(y_pos)) , 15 ,(0,255,0) , -1)
-        cv2.circle(img , ((x1+x2)//2 , (y1+y2)//2) , 15 ,(255,255,255) , 2)
-        
-        #print( round(((x2-x1) / (y2-y1)),2) == round((1920/1080),2) )
         resized = cv2.resize(img[y1:y2,x1:x2] , (self.width , self.height))
         
         return img ,resized

main.py
@@ -6,23 +6,31 @@ from video_stitching.video_stitcher import VideoStitcher
 from camera_motion_control.eptz_control import eptz
 from yolov5_detect import yolov5_detect
 
-from util import plot_one_box , timer ,ball_motion_detection
+from util import plot_one_box , timer ,ball_motion_detection ,eptz_random
 from math import sqrt
 
 class targeting(object):
-    def __init__(self , width ,height):
+    def __init__(self , 
+                width ,
+                height, 
+            ):
         self.x = width // 2
         self.y = height // 2
         
-        self.ball_motion_detection = ball_motion_detection( n=10 , thres=500 )
-        self.KMedoids_res = [0,0]
+        ############ 
+        # Parameters !!!
+        self.center_thres = 500
+        self.ball_player_dist_thres = 500
+        self.ball_missing_timeout = 120
 
-        self.center_thres = 200
-        self.ball_player_dist_thres = 200
-        self.ball_missing_timeout = 10
+        ball_sample_rate = 60
+        ball_energy_thresh = 40
+        ############
+        self.ball_motion_detection = ball_motion_detection( n = ball_sample_rate , thres = ball_energy_thresh )
+
         self.is_ball_exists = False
+        self.KMedoids_res = [0,0]
         self.ball_missing_count = 0
-
     def run(self,res ,img):
         self.img = img
         players = []
@@ -39,6 +47,7 @@ class targeting(object):
 
                     ball_pos = [(c[0] + c[2])//2,(c[1] + c[3])//2]
                     self.ball_motion_detection.update(ball_pos)
+                    cv2.putText(img, str(int(self.ball_motion_detection.ball_score)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX,2, (0, 255, 255), 2, cv2.LINE_AA)
                     if self.ball_motion_detection.check_score():
                         #print(self.KMedoids_res , ball_pos ,round(self.ball_motion_detection.ball_score), "target : ball")
                         #cv2.putText(stitched_img, "target : ball", (80, 80), cv2.FONT_HERSHEY_SIMPLEX,3, (0, 255, 255), 2, cv2.LINE_AA)
@@ -83,9 +92,9 @@ class targeting(object):
         else:
             center = centers[pred]
         
-        for i in centers:
-            cv2.circle(self.img ,i.astype(int) , 15,(0,0,255),-1) 
-        cv2.circle(self.img ,center.astype(int) , 15,(0,255,255),-1)
+        #for i in centers:
+        #    cv2.circle(self.img ,i.astype(int) , 20,(0,0,255),-1) 
+        #cv2.circle(self.img ,center.astype(int) , 15,(0,255,255),-1)
         return center.astype(int)
 
     def draw(self,res,stitched_img,color = (255,255,255)):
@@ -105,17 +114,30 @@ class targeting(object):
         return max(set(List), key = List.count)
 
 class autobroadcast(object):
-    def __init__(self , model_path , left_path ,right_path , camera_size=[1920,1080] , display = False):
+    def __init__(self , 
+                model_path , 
+                left_path  ,
+                right_path , 
+                camera_size=[1920,1080] ,
+                display = False ,
+                single_cam = False
+        ):
         self.cap_L = cv2.VideoCapture(left_path)
         self.cap_R = cv2.VideoCapture(right_path)
-        full_size = int(self.cap_L.get(3)*2) , int(self.cap_L.get(4))
-        self.length = int(self.cap_L.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.stitcher = VideoStitcher(fullsize=full_size)
+        if not single_cam:
+            full_size = int(self.cap_L.get(3)*2) , int(self.cap_L.get(4))
+        else:
+            full_size = int(self.cap_L.get(3)) , int(self.cap_L.get(4))
 
+        self.length = int(self.cap_L.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        self.stitcher = VideoStitcher(fullsize=full_size , initial_frame_count=20)
+        
         self.tg = targeting(width = full_size[0] , height=full_size[1])
         
         if cv2.imwrite("black.png" , np.zeros([full_size[0],full_size[1],3])):
-            self.detector = yolov5_detect(source="black.png",
+            self.detector = yolov5_detect(
+                                source="black.png",
                                 detect_mode='frame_by_frame',
                                 nosave=True,
                                 fbf_output_name="output",
@@ -124,22 +146,32 @@ class autobroadcast(object):
                                 half=True,
                                 fbf_close_logger_output=True
                             )
-        
-        self.eptz_control = eptz(camera_size , full_size)
+            self.detector.conf_thres = 0.5
+        self.eptz_control = eptz(
+                                size = camera_size, 
+                                fullsize = full_size,
+                                kp = 0.01,
+                                ki = 0.02,
+                                kd = 0.45,
+                                debug = True
+                            )
 
+        self.eptz_random = eptz_random()
+        self.random_value = full_size[0]//2 , full_size[1]//2 , 1.1
         self.target = full_size[0]//2 , full_size[1]//2
         self.display = display
         
     def run(self):
         count = 0
-        while(self.cap_L.isOpened() and self.cap_R.isOpened()):
+        while(self.cap_L.isOpened()):
             # skipping frames
             count +=1
+            """
             if count%4 != 0:
                 ret_L , frame_L = self.cap_L.read()
                 _     , frame_R = self.cap_R.read()
                 continue
-            
+            """
             ret_L , frame_L = self.cap_L.read()
             _     , frame_R = self.cap_R.read()
             
@@ -149,9 +181,10 @@ class autobroadcast(object):
                 t = timer()
 
                 #stitch
-                stitched_img = self.stitcher.stitch([frame_L ,frame_R]) #cpu : 52.0 ms  ,FPS : 19.059 ; gpu : 59.0 ms  ,FPS : 16.876 
+                #stitched_img = self.stitcher.stitch([frame_L ,frame_R]) #cpu : 52.0 ms  ,FPS : 19.059 ; gpu : 59.0 ms  ,FPS : 16.876 
+                stitched_img = frame_L
                 t.add_label("Image stitching")
-
+                
                 #Object detection
                 self.detector.run(stitched_img) 
                 res = self.detector.pred_np
@@ -161,9 +194,17 @@ class autobroadcast(object):
                 self.target = self.tg.run(res ,stitched_img)
                 self.tg.draw(res,stitched_img=stitched_img)
                 t.add_label("Targeting")
-
+                
                 # ePTZ
-                src , resized = self.eptz_control.run(stitched_img , 1.1, self.target[0] , self.target[1])
+
+                """
+                n = self.eptz_random.update()
+                if n:
+                    self.random_value = n
+                src , resized = self.eptz_control.run(stitched_img , self.random_value[2], self.random_value[0] , self.random_value[1])
+                """
+
+                src , resized = self.eptz_control.run(stitched_img , 1.5, self.target[0] , self.target[1])
                 t.add_label("ePTZ")
                 
                 t.show()
@@ -181,8 +222,8 @@ if __name__ == "__main__":
     #model_path = './models/0719_player_heavy.engine'
     #detector = object_detector(model_path=model_path, width=3840 , height=1080 , imgsz=(1280,320))
     
-    i = 53
-    left_path = f'./videos/vid_{i}/out_L.mp4'
+    i = 55
+    left_path = f'./videos/vid_{i}/out_R.mp4'
     right_path = f'./videos/vid_{i}/out_R.mp4'
 
     model_path = './models/0725_best_model.engine'
@@ -191,6 +232,7 @@ if __name__ == "__main__":
                         left_path = left_path,
                         right_path = right_path,
                         camera_size = [1920,1080],
-                        display=True)
+                        display=True,
+                        single_cam=True)
     bs.run()
             

video_stitching/video_stitcher.py
@@ -3,7 +3,7 @@ import cv2
 from time import time
 
 class VideoStitcher:
-    def __init__(self,fullsize):
+    def __init__(self,fullsize ,initial_frame_count = 20):
         # Initialize arguments
         self.saved_homo_matrix = None
         
@@ -14,29 +14,42 @@ class VideoStitcher:
         self.image_a = None
         self.image_b = None
         self.output_shape = None
+        
+        self.count = 1
+        self.initial_frame_count = initial_frame_count
+    def train_homography_mat(self,image_a , image_b,ratio=0.7, reproj_thresh=20.0):
+        
+        (keypoints_a, features_a) = self.detect_and_extract(image_a)
+        (keypoints_b, features_b) = self.detect_and_extract(image_b)
 
+        matched_keypoints = self.match_keypoints(keypoints_a, keypoints_b, features_a, features_b, ratio, reproj_thresh)
+
+        if matched_keypoints is None:
+            return None
+
+        if self.saved_homo_matrix is None:
+            self.saved_homo_matrix = matched_keypoints[1]
+        else:
+            self.saved_homo_matrix = (self.saved_homo_matrix * self.count + matched_keypoints[1]) / (self.count+1)
+            self.count +=1
     def stitch(self, images, ratio=0.7, reproj_thresh=20.0):
         (image_b, image_a) = images
         #print(image_a.dtype ,image_b.dtype)
-        if self.saved_homo_matrix is None:
-            (keypoints_a, features_a) = self.detect_and_extract(image_a)
-            (keypoints_b, features_b) = self.detect_and_extract(image_b)
-
-            matched_keypoints = self.match_keypoints(keypoints_a, keypoints_b, features_a, features_b, ratio, reproj_thresh)
-
-            if matched_keypoints is None:
-                return None
-
-            self.saved_homo_matrix = matched_keypoints[1]
         
         self.image_a = image_a
         self.image_b = image_b
+        if self.count < self.initial_frame_count :
+            #print(self.count)
+            self.train_homography_mat(image_a , image_b,ratio, reproj_thresh)
+        
+
         if (self.image_a is not None) and (self.output_shape is None):
             self.output_shape = (self.image_a.shape[1] + self.image_b.shape[1], self.image_a.shape[0])
         
         cv2.warpPerspective(self.image_a, self.saved_homo_matrix, self.output_shape , dst = self.result, flags=cv2.INTER_NEAREST) # 0.006200075149536133 ms
         s = time()
         self.result = self.blending(self.image_b , self.result) #0.06116604804992676 ms
+
         #print(f"{round(time() - s ,3) *1000} ms  ,FPS : { round(1 / (time() - s ) , 3)} ")
         #print(self.result)
         return self.result
@@ -64,7 +77,6 @@ class VideoStitcher:
             # Ensure the distance is within a certain ratio of each other (i.e. Lowe's ratio test)
             if len(raw_match) == 2 and raw_match[0].distance < raw_match[1].distance * ratio:
                 matches.append((raw_match[0].trainIdx, raw_match[0].queryIdx))
-        print(len(matches))
         # Computing a homography requires at least 4 matches
         if len(matches) > 4:
             # Construct the two sets of points
@@ -136,7 +148,7 @@ class VideoStitcher:
         (x, y, w, h) = cv2.boundingRect(cnts[id]) 
         mid_line = (img.shape[1]//2)
         overlap_mid_line  = (mid_line + x)//2
-        constant = 100
+        constant = 50
         #mask1 = np.repeat(np.tile(np.linspace(0, 1, mid_line - x ), (img.shape[0], 1))[:, :, np.newaxis], 1, axis=2)[:,:,0]
         mask2 = np.repeat(np.tile(np.linspace(0, 1, constant), (img.shape[0], 1))[:, :, np.newaxis], 1, axis=2)[:,:,0]
         img[:,overlap_mid_line:] = 0